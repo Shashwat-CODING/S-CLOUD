144f516fab32338b5dd3bbe3f750a92e2e464360
Shashwat-CODING <bob17040246@gmail.com>
2024-10-02 09:25:12 +0530

    Create artist.js

---
+++ artist.js
@@ -0,0 +1,13 @@
+const sampleArtists = [
+        { name: "Sidhu Moose Wala", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmqV--ZswiBYE51mONPtphc6iWd6CM8t8y5w&s" },
+        { name: "Shubh", image: "https://yt3.googleusercontent.com/VkuOD8b6D6E3ayAOpDeWztKrViN5Zdbn6w7UHl7NoSRny7ZLYkMJUT22ll6JnHh3SZwY8nHUTA=s900-c-k-c0x00ffffff-no-rj" },
+        { name: "Yo Yo Honey Singh", image: "https://i.scdn.co/image/ab67616100005174bc7e4fffd515b47ff1ebbc1f" },
+        { name: "Mashup", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtCz6Ec3EyW2KZ3ZBQNwjqDsjNXhuKqvj8Vw&s"},
+        { name: "Arjit Singh", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQEaO9oNwP0hT6rCxrP1_xtB2Q_ORBTsB3cmw&s" },
+        { name: "Parmish Verma", image: "https://artistsimages.b-cdn.net/parmish-verma/parmish-verma-1.jpg" },
+        { name: "Prem Dhillon", image: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/Prem_Dhillon_Majha_Block.jpg/640px-Prem_Dhillon_Majha_Block.jpg" },
+        { name: "Sonu Nigam", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSt7iziufhnBLORGBl3XpAvLWJe-yl_853OQ&s" },
+        { name: "Diljit Dosanjh", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2zt0VtorPuPmbB4AiR4SP3l1jcDrLLT3ZGQ&s" },
+        { name: "Udit Narayan", image: "https://m.economictimes.com/thumb/msid-94687190,width-1200,height-900,resizemode-4,imgsize-29224/udit-narayan-canva.jpg" },
+        { name: "Shreya Ghoshal", image: "https://i.scdn.co/image/ab6761610000517436a7262247318ff0f1ad00da" }
+    ];
